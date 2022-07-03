fe6f44e6d51ea38b4e69a257db2e1d0be39d46ae
Kuba <ksulaymanov@gmail.com>
2022-07-02 19:41:46 -0500

    added choice select in article create

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from .models import Article
+from .models import Article, STATUS_CHOICES
 
 
 def article_view(request):
@@ -24,12 +24,13 @@ def index_view(request):
 
 def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        return render(request, 'article_create.html', {'statuses': STATUS_CHOICES})
     else:
         title = request.POST.get('title')
         content = request.POST.get('content')
+        status = request.POST.get('status')
         author = request.POST.get('author')
-        new_art = Article.objects.create(title=title, author=author, content=content)
+        new_art = Article.objects.create(title=title, author=author, content=content, status=status)
         context = {'article': new_art}
         return render(request, 'article_view.html', context)
 
