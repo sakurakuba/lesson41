b80c8fe94323291927d91db220896a5c042ca285
Kuba <ksulaymanov@gmail.com>
2022-07-02 19:16:00 -0500

    added choices model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [('new', 'brand new'), ('moderated', 'updated'), ('rejected', 'declined')]
+
+
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Title name')
     author = models.CharField(max_length=50, verbose_name='Author', default='Unknown')
     content = models.TextField(max_length=3000, verbose_name='Content')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Create date')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name='Status', default=STATUS_CHOICES[0][0])
 
     def __str__(self):
         return f"{self.id}. {self.title}: {self.author}"
