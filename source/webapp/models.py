c64bae1d63526a36e8facc090022ac1fb96a8e75
Kuba <ksulaymanov@gmail.com>
2022-07-10 18:55:28 -0500

    added comments class and new migration

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,12 +4,18 @@ from django.db import models
 STATUS_CHOICES = [('new', 'brand new'), ('moderated', 'updated'), ('rejected', 'declined')]
 
 
-class Article(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Create date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
+
+    class Meta:
+        abstract = True
+
+
+class Article(BaseModel):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Title name')
     author = models.CharField(max_length=50, verbose_name='Author', default='Unknown')
     content = models.TextField(max_length=3000, verbose_name='Content')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Create date')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name='Status', default=STATUS_CHOICES[0][0])
 
     def __str__(self):
@@ -19,3 +25,19 @@ class Article(models.Model):
         db_table = 'articles'
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
+
+
+class Comment(BaseModel):
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    article = models.ForeignKey("webapp.Article", on_delete=models.CASCADE, related_name="comments", verbose_name="article")
+
+    def __str__(self):
+        return f"{self.id}. {self.text}: {self.author}"
+
+    class Meta:
+        db_table = "comments"
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+
+
