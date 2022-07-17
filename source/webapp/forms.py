b6e8db3198149cf672f1bd85ea71a1eb8572617e
Kuba <ksulaymanov@gmail.com>
2022-07-17 01:34:59 -0500

    added form for create and update article

---
+++ source/webapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import widgets
+
+from .models import Article, STATUS_CHOICES
+
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(max_length=50, required=True, label='Title name')
+    author = forms.CharField(max_length=50, required=True, label='Author')
+    content = forms.CharField(max_length=3000, required=False, label='Content', widget=widgets.Textarea(attrs={"cols": 15, "rows": 3}))
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=True, label='Status')
+
+    class Meta:
+        model = Article
+        fields = "__all__"
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView, RedirectView
 
+from .forms import ArticleForm
 from .models import Article, STATUS_CHOICES
 
 
@@ -25,11 +26,6 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.order_by('-created_at')
         context = {'articles': articles}
-        # print(request.GET)
-        # print(request.GET.getlist('my_param'))
-        # print(request.GET.get('my_param', 10))
-        # print(request.GET.get('other_param'))
-        # print(request.GET.get('other_param', 10))
         return render(request, "index.html", context)
 
 
@@ -52,30 +48,40 @@ class MyRedirectView(RedirectView):
 
 def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html', {'statuses': STATUS_CHOICES})
+        form = ArticleForm()
+        return render(request, 'article_create.html', {'statuses': STATUS_CHOICES, 'form': form})
     else:
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        ## status = request.POST.get('status')
-        author = request.POST.get('author')
-        new_art = Article.objects.create(title=title, author=author, content=content)  # status=status)
-        context = {'article': new_art}
-        ## return HttpResponseRedirect(f"/article/{new_art.pk}")
-        # return render(request, 'article_view.html', context)
-        ### return HttpResponseRedirect(reverse('article_view', kwargs={'pk': new_art.pk}))
-        return redirect('article_view', pk=new_art.pk)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            author = form.cleaned_data.get('author')
+            content = form.cleaned_data.get('content')
+            status = form.cleaned_data.get('status')
+            new_art = Article.objects.create(title=title, author=author, content=content, status=status)
+            return redirect('article_view', pk=new_art.pk)
+        return render(request, 'article_create.html', {'form': form})
+
 
 
 def update_article(request, pk):
     article = get_object_or_404(Article, pk=pk)
     if request.method == 'GET':
-        return render(request, 'update.html', {'article': article})
+        form = ArticleForm(initial={
+            'title': article.title,
+            'author': article.author,
+            'content': article.content,
+            'status': article.status
+        })
+        return render(request, 'update.html', {'form': form})
     else:
-        article.title = request.POST.get('title')
-        article.content = request.POST.get('content')
-        article.author = request.POST.get('author')
-        article.save()
-        return redirect('article_view', pk=article.pk)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data.get('title')
+            article.content = form.cleaned_data.get('content')
+            article.author = form.cleaned_data.get('author')
+            article.status = form.cleaned_data.get('status')
+            article.save()
+            return redirect('article_view', pk=article.pk)
 
 
 def delete_article(request, pk):
