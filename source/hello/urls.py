2b99ab3b350d0947b1f6b13b69541c58fc8163e5
Kuba <ksulaymanov@gmail.com>
2022-07-17 16:34:15 -0500

    added requirements and fixed article view

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('articles/', RedirectView.as_view(pattern_name='index')),
     path('articles/add/', article_create_view, name='create_article'),
-    path('article/<int:pk>/', ArticleView.as_view(extra_context={"test": 7}), name='article_view'),
+    path('article/<int:pk>/', ArticleView.as_view(extra_context={"test": "extra content"}), name='article_view'),
     path('article/<int:pk>/update', UpdateArticle.as_view(), name='update_article'),
     path('article/<int:pk>/delete', delete_article, name='delete_article'),
     path('google/', MyRedirectView.as_view())
