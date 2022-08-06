3f77825f6c5b12e89d116cb3a24d1931354f3d1a
Kuba <ksulaymanov@gmail.com>
2022-08-06 17:34:56 -0500

    added functionality to create user, validate registration form. updated base, login pages

---
+++ source/accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class MyUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm password", required=True, widget=forms.PasswordInput, strip=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords does not match!')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from accounts.forms import MyUserCreationForm
+
+
+def register_view(request):
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    return render(request, 'user_create.html', context={'form': form})
+
+
+
 
 def login_view(request):
     context = {}
