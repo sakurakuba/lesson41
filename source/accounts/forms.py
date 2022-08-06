8e47942a415ac229321991cb1ebd0884108922c3
Kuba <ksulaymanov@gmail.com>
2022-08-06 17:46:54 -0500

    refactored MyUserCreationForm to use from django.contrib.auth.forms

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,25 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
-class MyUserCreationForm(forms.ModelForm):
-    password = forms.CharField(label="Password", strip=False, required=True, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Confirm password", required=True, widget=forms.PasswordInput, strip=False)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_confirm = cleaned_data.get("password_confirm")
-        if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError('Passwords does not match!')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
-
+class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
+        fields = ['username', 'password1', 'password2',
+                  'first_name', 'last_name', 'email']
+        field_classes = {'username': UsernameField}
+
+
+# class MyUserCreationForm(forms.ModelForm):
+#     password = forms.CharField(label="Password", strip=False, required=True, widget=forms.PasswordInput)
+#     password_confirm = forms.CharField(label="Confirm password", required=True, widget=forms.PasswordInput, strip=False)
+#
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data.get("password")
+#         password_confirm = cleaned_data.get("password_confirm")
+#         if password and password_confirm and password != password_confirm:
+#             raise forms.ValidationError('Passwords does not match!')
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.set_password(self.cleaned_data["password"])
+#         if commit:
+#             user.save()
+#         return user
+#
+#     class Meta:
+#         model = User
+#         fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
